14dfc9377d3cd89f784d9bb396899ff81ff5c914
martel1124 <760386482@qq.com>
2022-08-29 17:25:00 +0800

    Add:中国地图

---
+++ components/地理地图/src/main.js
@@ -0,0 +1,9 @@
+/**
+ * @description 注册组件到大屏中
+ */
+
+import { registerComponent } from 'data-vi/components';
+
+import Component from './Component';
+
+registerComponent('61aa27abd39bdf74f6d60149', 'v-current',  Component);
